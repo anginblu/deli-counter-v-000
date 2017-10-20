d8511b5f6696493569f6397a7d1425dee4b3546e
Jacqueline Lee <anginblu@gmail.com>
2017-10-20 02:32:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def line(array)
+  if array.size = 0
+    "The line is currently empty."
+  else
+    array.each do |name| 
+      index = array.index(name)
+      number = index + 1
+      array(index) = "number. " + name
+    end
+    array.join(" ")
+    "The line is currently: #{array}"
+    
+  
